0efac092717c1a90e0e58b1b19bcb192c64c4082
AnatolySP <anspesiv@yahoo.com>
2019-11-20 14:52:17 +0300

    added empty picture functionality to blog view

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,8 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/blog/')
+    image = models.ImageField(upload_to='images/blog/', blank=True)
+
+    def summary(self):
+        '''This is shortening of the body text, like a preview'''
+        return " ".join(self.body.split()[:20])+"..."
