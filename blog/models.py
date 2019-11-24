8d32b337cb3dd09ab12b0bc5462039f454966105
AnatolySP <anspesiv@yahoo.com>
2019-11-24 17:31:10 +0300

    detailed blog engine added, the beginning

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,12 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
 
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def mod_date_pretty(self):
+        return self.mod_date.strftime('%b %e %Y, %H:%M')
+
     def summary(self):
-        '''This is shortening of the body text, like a preview'''
+        '''This is shortening of the body text, like a small preview... currently it takes first 20 words.'''
         return " ".join(self.body.split()[:20])+"..."
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
@@ -8,5 +8,9 @@ def allblogs(request):
     for i,k in blog:
         blogdict[i]=k
 '''
+    return render(request, 'blog/allblogs.html', {'blog': blog})
 
-    return render(request, "blog/allblogs.html", {'blog': blog})
+def detail(request, blog_id):
+    '''This forms a detailed page for a specific blog'''
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'detailblog':detailblog})
