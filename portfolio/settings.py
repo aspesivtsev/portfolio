10d4a6170d62616e4118a0b959ada8ae35b50378
AnatolySP <anspesiv@yahoo.com>
2019-12-06 15:51:48 +0300

    final release, all set and works. new djangodbman account is used

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'31459',
+        'USER':'djangodbman',
+        'PASSWORD':'django1234',
         'HOST':'localhost',
         'PORT':'5432',
     }
